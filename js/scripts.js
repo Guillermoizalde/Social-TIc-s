1909b17b2d7d53cf0e277a3d37b8152a218c7cb7
Guillermo Izalde <izalde.guillermo45@gmail.com>
2024-12-04 18:33:53 -0700

    Primero

---
+++ js/scripts.js
@@ -0,0 +1,42 @@
+/*!
+* Start Bootstrap - New Age v6.0.7 (https://startbootstrap.com/theme/new-age)
+* Copyright 2013-2023 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-new-age/blob/master/LICENSE)
+*/
+//
+// Scripts
+// 
+new QRCode(document.getElementById("qrcode"), {
+    text: "https://github.com/", // Cambia esto por la URL de tu PWA o el link de descarga
+    width: 128,
+    height: 128,
+    colorDark: "#4caf50",
+    colorLight: "#ffffff",
+    correctLevel: QRCode.CorrectLevel.H
+});
+
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Activate Bootstrap scrollspy on the main nav element
+    const mainNav = document.body.querySelector('#mainNav');
+    if (mainNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#mainNav',
+            offset: 74,
+        });
+    };
+
+    // Collapse responsive navbar when toggler is visible
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
+
+});
---
+++ manifest.js
@@ -0,0 +1,22 @@
+{
+    "name": "Social tics",
+    "short_name": "Social tics",
+    "description": "Una red social para las y los guerreros UTT.",
+    "start_url": "/index.html",  
+    "display": "standalone",  
+    "background_color": "#ffffff",
+    "theme_color": "#007bff",  
+    "icons": [
+      {
+        "src": "images/icon-192x192.png", 
+        "sizes": "192x192",
+        "type": "image/png"
+      },
+      {
+        "src": "images/icon-512x512.png", 
+        "sizes": "512x512",
+        "type": "image/png"
+      }
+    ]
+  }
+  
